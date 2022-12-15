c357e775a43ae9224c551be6a5a72a7a9e4004e5
pkulium <54947136+pkulium@users.noreply.github.com>
2022-12-15 13:16:48 -0600

    Update client.py

--- client.py
+++ client.py
@@ -33,7 +33,7 @@ class Client():
         for epoch in range(1000):
             for data in self.train_loader:
                 inputs, labels = data
-                if not self.honest:
+                if self.id >= 100:
                     labels.apply_(lambda x: 7)
                 inputs = Variable(inputs).to(device)
                 labels = Variable(labels).to(device)
