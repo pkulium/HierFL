99ff0c8c72b5005da59b0778dc50c2b1aa41010c
pkulium <54947136+pkulium@users.noreply.github.com>
2022-12-22 10:29:17 -0600

    Update options.py

--- options.py
+++ options.py
@@ -166,6 +166,14 @@ def args_parser():
         default=0,
         type=int
     )
+    
+    parser.add_argument(
+        '--num_honest_clients',
+        type = int,
+        default = 10,
+        help = 'number of all honest clients'
+    )
+
 
 
     args = parser.parse_args()
