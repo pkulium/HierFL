326d585940b821a9555af8b63e65590277fd2fc4
pkulium <54947136+pkulium@users.noreply.github.com>
2022-12-31 08:53:35 -0600

    Update average.py

--- average.py
+++ average.py
@@ -9,6 +9,8 @@ def average_weights(w, s_num):
     w_avg = copy.deepcopy(w[0])
     for k in w_avg.keys():  #the nn layer loop
         for i in range(1, len(w)):   #the client loop
-            w_avg[k] += torch.mul(w[i][k], torch.tensor(s_num[i]/temp_sample_num, dtype=torch.float))
-        w_avg[k] = torch.mul(w_avg[k], torch.tensor(temp_sample_num/total_sample_num, dtype=torch.float))
+            delta = torch.mul(w[i][k], s_num[i]/temp_sample_num)
+            print((w_avg[k].dtype, delta.dtype))
+            w_avg[k] += delta
+        w_avg[k] = torch.mul(w_avg[k], temp_sample_num/total_sample_num)
     return w_avg
